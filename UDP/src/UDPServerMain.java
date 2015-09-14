c40462bdda925a7b5f835c2f5975bdfe4210bf4a
Mirim <mirim@e-mirim.hs.kr>
2015-09-14 17:05:31 +0900

    add UDPServerMain

--- UDP/src/UDPServerMain.java
+++ UDP/src/UDPServerMain.java
@@ -1,11 +1,26 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 /**
  * Created by Mirim on 2015-09-10.
  */
 public class UDPServerMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        byte[] data = new byte[1024];
+
         //make socket
+        DatagramSocket ds = new DatagramSocket(1226);
+
         //make packet
+        DatagramPacket dp = new DatagramPacket(data, data.length);
+
         //receive
+        System.out.println("UDP Server standby...");
+        ds.receive(dp);
+
         //print message
+        System.out.println(dp.getData());
     }
 }
