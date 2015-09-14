f2c51f13b91dca96d8d2f9f41046f2255de42c1a
Mirim <mirim@e-mirim.hs.kr>
2015-09-14 16:56:03 +0900

    add UDPClientMain

--- UDP/src/UDPClientMain.java
+++ UDP/src/UDPClientMain.java
@@ -1,17 +1,29 @@
+import java.io.IOException;
+import java.net.*;
 import java.util.Scanner;
 
 /**
  * Created by Mirim on 2015-09-10.
  */
 public class UDPClientMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String message = "";
         //input message
         System.out.println("보낼 메시지> ");
         Scanner scanner = new Scanner(System.in);
         message = scanner.nextLine();
+
         //make packet
+        DatagramPacket dp = new DatagramPacket(
+                message.getBytes(),
+                message.getBytes().length,
+                InetAddress.getByName("localhost"),
+                1226);
+
         //make socket
+        DatagramSocket ds = new DatagramSocket();
+
         //send
+        ds.send(dp);
     }
 }
