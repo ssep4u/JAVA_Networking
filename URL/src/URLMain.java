ddecf8375518562939d7ecc6743fde94933ecae1
Mirim <mirim@e-mirim.hs.kr>
2015-09-24 17:24:13 +0900

    add URLMain + GUI

--- URL/src/URLMain.java
+++ URL/src/URLMain.java
@@ -1,5 +1,11 @@
+import java.awt.*;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Mirim on 2015-09-24.
@@ -12,11 +18,55 @@ public class URLMain {
         StringBuffer sb = new StringBuffer();
 
         //URL
+        //블로터 : http://feeds.feedburner.com/bloter
+        //TheVerge : http://www.theverge.com/rss/frontpage
+        String urlString = "http://www.theverge.com/rss/frontpage";
         //make Reader
-        //read
-        //print
+        try {
+            url = new URL(urlString);
+            br = new BufferedReader(new InputStreamReader(url.openStream(),"UTF-8"));
+            //read
+            while((line = br.readLine()) != null) {
+                //print
+//                System.out.println(line);
+                sb.append(line);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         //make GUI
+        Frame frame = new Frame("Bloter");
+        List list = new List();
+        frame.add(list);
+
         //pattern matching
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>");
+        Matcher matcher = pattern.matcher(sb.toString());
+        while(matcher.find()) {
+//            System.out.println(matcher.group(1).trim());
+            list.add(matcher.group(1).trim());
+        }
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
     }
 }
+//Run > Edit Configuration...
+// > URLMain > Configuration
+// > VM options: -Dfile.encoding=MS949
+
+
+
+
+
+
+
+
+
+
+
+
+
